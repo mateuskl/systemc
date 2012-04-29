491b726c033ce19c1ede35407a34800fec4ad19b
Mateus Krepsky Ludwich <mkludwich@gmail.com>
2012-04-29 01:55:06 -0300

    Test bench for half adder.

---
+++ designs/adder/design/test_bench.cc
@@ -0,0 +1,49 @@
+#include <test_bench.hh>
+
+#include <half_adder.hh>
+
+#include <iostream>
+
+using namespace std;
+
+void __init()
+{
+    // S = A XOR B
+    __expected_S[0][0] = 0;
+    __expected_S[0][1] = 1;
+    __expected_S[1][0] = 1;
+    __expected_S[1][1] = 0;
+    
+    // C = A AND B
+    __expected_C[0][0] = 0;
+    __expected_C[0][1] = 0;
+    __expected_C[1][0] = 0;
+    __expected_C[1][1] = 1;
+}
+
+
+void HalfAdderTestBench::generate_stimulus()
+{
+    for (int ia = 0; ia < 2; ia++) 
+    {
+        for (int ib = 0; ib < 2; ib++)
+        {
+            stimulus_a.write(ia);
+            stimulus_b.write(ib);
+            __ia = ia;
+            __ib = ib;
+            wait(__checked);      
+        }
+    }
+    
+    sc_stop();
+}
+
+
+void HalfAdderTestBench::check()
+{
+    cout << "A: " << __ia << " B: " << __ib << " => " <<  "expS: " << __expected_S[__ia][__ib] << " obtS: " << obtained_s.read() << " expC: " << __expected_C[__ia][__ib] << " obtC: " << obtained_c.read();
+    assert(__expected_S[__ia][__ib] == obtained_s.read());
+    assert(__expected_C[__ia][__ib] == obtained_c.read());
+    __checked.signal();  
+}
---
+++ designs/adder/design/test_bench.hh
@@ -0,0 +1,41 @@
+#include <systemc.h>
+ 
+SC_MODULE(HalfAdderTestBench)   
+{
+    
+    // stimulus
+    sc_out<sc_bit> stimulus_a;
+    sc_out<sc_bit> stimulus_b;
+    
+    // obtained values
+    sc_in<sc_bit> obtained_s;
+    sc_int<sc_bit> obtained_c;
+ 
+ 
+    void generate_stimulus();
+    void check();
+ 
+ 
+    SC_CTOR(HalfAdderTestBench)
+    {
+        __init();
+        SC_THREAD(generate_stimulus);
+        
+        
+        SC_METHOD(check);
+        sensitive << obtained_s << obtained_c; 
+    }
+
+
+private:
+    void __init();
+    
+private:
+    sc_event __checked;
+
+    sc_bit __expected_S[2][2];
+    sc_bit __expected_C[2][2];
+    int __ia;
+    int __ib;
+    
+};
