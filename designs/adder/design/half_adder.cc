9211fb05e0c9d000bd4553c3eeaaf8ffe3b0c9fa
mateus <mateus@kenobi.none>
2012-04-30 02:05:52 -0300

    traces

--- designs/adder/design/half_adder.cc
+++ designs/adder/design/half_adder.cc
@@ -1,10 +1,16 @@
 #include <half_adder.hh>
 
+#include <iostream>
+
 // process
 void HalfAdder::do_add()
 {
+    std::cout << "HalfAdder::do_add\n";
+    
     // S = A XOR B
-    S.write(A.read() ^ B.read());
+    S.write((A.read() ^ B.read()));
+    
+    std::cout << "S: " << S.read() << " A: " << A.read() << " B: " << B.read() << " A XOR B: " << (A.read() ^ B.read())  << std::endl; 
     
     // C = A AND B
     C.write(A.read() & B.read());
