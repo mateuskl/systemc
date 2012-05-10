41bcef102fa08f372c5af61c07837e45d6a36dfe
Mateus Krepsky Ludwich <mkludwich@gmail.com>
2012-05-09 23:35:47 -0300

    AND gates works!

--- designs/and_gate/top_level.cc
+++ designs/and_gate/top_level.cc
@@ -1,28 +1,88 @@
 #include <systemc.h>
 
+#include <iostream>
+
 #include "and_gate.hh"
 
-#define TRACE 0
+#define TRACE 1
 
-int sc_main(int argc, char *argv[])
+SC_MODULE(TopLevelAndGateSimulation)
 {
+    int iter;
+    
+    sc_time clk_period;    
+    sc_clock clock;    
+    
+    AndGate andgate;
+    
+    sc_in<bool> clk_in;
+    
     sc_signal<bool> A;
     sc_signal<bool> B;
     sc_signal<bool> S;
 
-    AndGate andgate("Adder");
     
-    andgate.A(A);
-    andgate.B(B);
-    andgate.S(S);
+    void compute()
+    {
+        std::cout << "compute\n";
+        
+        A.write(false);
+        B.write(true);
+        
+        wait(clk_in.posedge_event());
+        wait(clk_in.posedge_event());
+        wait(clk_in.posedge_event());
+        
+        A.write(true);
+        B.write(true);       
+    }
+    
+    void doand()
+    {       
+        std::cout << "doand: " << iter << "\n";
+        
+        if (iter < 10)
+        {
+            iter ++;
+        }
+        else
+        {
+            sc_stop();
+        }
+    }
+    
+    
+    SC_CTOR(TopLevelAndGateSimulation) :
+        clock("clock"),
+        andgate("Adder"),
+        iter(0),
+        clk_period(30, SC_NS)
+    {
+        
+        andgate.A(A);
+        andgate.B(B);
+        andgate.S(S);
+        clk_in(clock);
+
+        SC_METHOD(doand);
+        sensitive << clk_in.pos();
+        
+        SC_THREAD(compute);
+    }
+};
+
+int sc_main(int argc, char *argv[])
+{
+    TopLevelAndGateSimulation top("TopLevel");
     
 #if TRACE    
     sc_trace_file *trcf = sc_create_vcd_trace_file("trace");
     if (trcf) 
     {
-        sc_trace(trcf, A, "A");
-        sc_trace(trcf, B, "B");
-        sc_trace(trcf, S, "S");
+        sc_trace(trcf, top.clk_in, "clk_in");
+        sc_trace(trcf, top.A, "A");
+        sc_trace(trcf, top.B, "B");
+        sc_trace(trcf, top.S, "S");
         trcf->set_time_unit(-15);
     } else 
     {
@@ -30,7 +90,8 @@ int sc_main(int argc, char *argv[])
     }
 #endif
     
-    sc_start(0);
+    //sc_start(0);
+    sc_start();
     
 
 #if TRACE    
